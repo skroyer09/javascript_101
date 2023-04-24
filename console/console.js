37b1ea6e414115cef1951524c93bcada5424da1b
steffen kroyer <stef5450@stud.kea.dk>
2023-04-24 14:56:46 +0200

    stertet working on JS 101

---
+++ console/console.js
@@ -0,0 +1,5 @@
+console.log(30);
+
+console.log(69);
+
+// This example logs 30 & 69 to the console. The semicolon denotes the end of the line, or statement.
---
+++ data_types/data_type.js
@@ -0,0 +1,9 @@
+console.log("JavaScript");
+console.log(2011);
+console.log("Woohoo! I love to code! #codecademy");
+console.log(20.49);
+
+/* 
+In the example above, we first printed a string. 
+Our string isn’t just a single word; it includes both capital and lowercase letters, spaces, and punctuation.
+Next, we printed the number 2011, notice we did not use quotes. */
