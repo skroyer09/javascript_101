db4c1dec5ef378532d47809008f6ebc412cebc00
steffen kroyer <stef5450@stud.kea.dk>
2023-04-26 14:09:14 +0200

    added if and else statements

---
+++ conditonal_statements/if_else.js
@@ -0,0 +1,24 @@
+let sale = true;
+
+sale = false;
+
+if (sale) {
+  console.log("Time to buy!");
+} else {
+  console.log("Time to wait for a sale.");
+}
+
+/* An else statement must be paired with an if statement, 
+and together they are referred to as an if...else statement.
+
+In the example above, the else statement:
+
+Uses the else keyword following the code block of an if statement.
+
+Has a code block that is wrapped by a set of curly braces {}.
+
+The code inside the else statement code 
+block will execute when the if statement’s condition evaluates to false.
+
+if...else statements allow us to automate solutions to yes-or-no questions, also known as binary decisions.
+*/
